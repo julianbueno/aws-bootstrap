525b42d5dd8748757974dac58c861d31b17443d0
Julian Bueno <julian.bueno@gmail.com>
2015-03-17 19:51:19 +1100

    Update teamcity-agent-magento.sh

--- teamcity-agent-magento.sh
+++ teamcity-agent-magento.sh
@@ -22,3 +22,45 @@ sudo apt-get install git
 #composer
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
+
+#install TC agent
+sudo apt-get install openjdk-7-jre-headless
+wget http://ec2/update/buildAgent.zip
+sudo apt-get install unzip
+(cd /var; sudo mkdir TeamCity; cd TeamCity)
+sudo unzip ~/buildAgent.zip
+(cd bin; sudo chmod +x agent.sh)
+(cd ../conf; sudo cp buildAgent.dist.properties buildAgent.properties)
+
+# We need to configure the agent
+### Change the serverUrl and name
+sudo nano buildAgent.properties
+#Add the following
+sudo nano /etc/init.d/teamcity
+###########################
+#! /bin/sh
+# /etc/init.d/teamcity 
+# Carry out specific functions when asked to by the system
+case "$1" in
+  start)
+    echo "Starting script teamcity "
+    /var/TeamCity/bin/agent.sh start
+    ;;
+  stop)
+    echo "Stopping script teamcity"
+    /var/TeamCity/bin/agent.sh stop
+    ;;
+  *)
+    echo "Usage: /etc/init.d/teamcity {start|stop}"
+    exit 1
+    ;;
+esac
+
+exit 0
+#############################
+sudo chmod +x /etc/init.d/teamcity
+sudo update-rc.d teamcity defaults
+
+# You can always start/stop the agent manually
+sudo /var/TeamCity/bin/agent.sh stop
+sudo /var/TeamCity/bin/agent.sh start
