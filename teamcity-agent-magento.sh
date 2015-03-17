87dec441bd4838d05a7728b457bb21767bc163c5
Julian Bueno <julian.bueno@gmail.com>
2015-03-17 17:34:52 +1100

    Update teamcity-agent-magento.sh

--- teamcity-agent-magento.sh
+++ teamcity-agent-magento.sh
@@ -4,3 +4,21 @@
 
 sudo apt-get update
 sudo apt-get upgrade
+
+#mysql
+sudo apt-get install mysql-server-5.6
+sudo apt-get install php5-mysql
+
+#php
+sudo add-apt-repository ppa:ondrej/php5
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install php5
+
+#git
+sudo apt-get update
+sudo apt-get install git
+
+#composer
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
