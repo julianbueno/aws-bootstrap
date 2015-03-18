6fae2090be884a5d139d66505146db2524f832a2
Julian Bueno <julian.bueno@gmail.com>
2015-03-18 12:19:00 +1100

    Create magento-dev.sh

---
+++ magento-dev.sh
@@ -0,0 +1,7 @@
+#git
+sudo apt-get update
+sudo apt-get install git
+
+bash < <(wget -q --no-check-certificate -O - https://raw.github.com/colinmollenhour/modman/master/modman-installer)
+export PATH=$PATH:/home/bitnami/bin/
+
