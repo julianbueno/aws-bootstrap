77304f1aa4d80303f37bd0e89682b946cfd43713
Julian Bueno <julian.bueno@gmail.com>
2015-03-18 12:39:11 +1100

    Update magento-dev.sh

--- magento-dev.sh
+++ magento-dev.sh
@@ -5,3 +5,8 @@ sudo apt-get install git
 bash < <(wget -q --no-check-certificate -O - https://raw.github.com/colinmollenhour/modman/master/modman-installer)
 export PATH=$PATH:/home/bitnami/bin/
 
+cd home/bitnami/apps/magento/htdocs
+
+modman init
+
+modman clone git@github.com:zipMoney/magento-gateway.git --branch feature/modman
